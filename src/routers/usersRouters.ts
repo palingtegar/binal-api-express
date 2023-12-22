42cbd702b9a77e8772a7d196e94b3062bbf7a827
palingtegar <tegar.itmamul@gmail.com>
2023-12-22 14:49:06 +0700

    feat: add login feature

--- src/routers/usersRouters.ts
+++ src/routers/usersRouters.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { registerUserController } from "../controllers/users/registerUserController";
+import { loginUserController } from "../controllers/users/loginUSerController";
 
 const router = express.Router();
 
 router.post("/register", registerUserController);
+router.post("/login", loginUserController);
 export default router;
