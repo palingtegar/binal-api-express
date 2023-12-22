c0070a19bd01d0c84fbea59ee1e68e37f61e5ca6
palingtegar <148305220+palingtegar@users.noreply.github.com>
2023-12-22 15:33:47 +0700

    feat: add hashing password (#3)

--- src/actions/users/registerUserAction.ts
+++ src/actions/users/registerUserAction.ts
@@ -3,12 +3,13 @@
 // kalau ada langsung return email/ username telah digunakan
 //kalau tidak ada yang menggunakan email/ username tsb create ke database
 
+import { hashPassword } from "../../helper/bcrypt";
 import { createUser } from "../../repositories/users/createUser";
 import { findUsersbyEmailAndUsername } from "../../repositories/users/findUsersbyEmailAndUsername";
 import { IUser } from "../../types/user.type";
 export const registerUserAction = async (data: IUser) => {
   try {
-    const { email, username } = data;
+    const { email, username, password } = data;
     const users = await findUsersbyEmailAndUsername(email, username);
 
     if (users.length) {
@@ -18,7 +19,9 @@ export const registerUserAction = async (data: IUser) => {
       };
     }
     //create user di database berarti harus masukin data/ query data ke database
-
+    //nah untuk enhance fiturnya, pake hashing
+    const hashedPassword = await hashPassword(password);
+    data.password = hashedPassword;
     await createUser(data);
     return {
       status: 200,
