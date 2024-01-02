3bbaf0c72d9f2b4ce6d5b5e2db11ced7c8f71ad2
palingtegar <148305220+palingtegar@users.noreply.github.com>
2024-01-02 23:58:12 +0700

    Get tweets orderBy desc (#10)
    * feat: add feature get tweet
    
    * feature: add feature orderby desc

--- src/repositories/tweets/getTweets.ts
+++ src/repositories/tweets/getTweets.ts
@@ -3,6 +3,9 @@ import prisma from "../../helper/prisma";
 export const getTweets = async () => {
   try {
     const tweets = await prisma.tweets.findMany({
+      orderBy: {
+        id: "desc",
+      },
       include: {
         User: true,
       },
